2298c0c16cf4edcb8848b795e9b549b781612e42
unknown <kaithwas.697@gmail.com>
2022-03-10 18:28:32 +0530

    Added Monthly wage of employee

--- CalculateEmployeeWage/CalculateEmployeeWage/Program.cs
+++ CalculateEmployeeWage/CalculateEmployeeWage/Program.cs
@@ -7,31 +7,35 @@ namespace CalculateEmployeeWage
         public const int empRatePrHr = 20;
         public const int isFullTime = 1;
         public const int isPartTime = 2;
+        public const int numOfDays = 20;
 
         static void Main(string[] args)
         {
-            // UC4 Part time Employees wage using switch statement
+            // UC5 Employees monthly wage using switch statement
             Console.WriteLine("Welcome to Employee Wage Computation");
 
-            int workHr;                     
-            int totalWage;            
-
-            int employeeStatus = new Random().Next(0, 2);  
-            
-            switch (employeeStatus)
+            int workHr;
+            int empWage;
+            int totalWage = 0;
+            for (int days = 0; days < numOfDays; days++)
             {
-                case isFullTime:
-                    workHr = 8;
-                    break;
-                case isPartTime:
-                    workHr = 4;
-                    break;
-                default:
-                    workHr = 0;
-                    break;
+                int employeeStatus = new Random().Next(0, 2);
+                switch (employeeStatus)
+                {
+                    case isFullTime:
+                        workHr = 8;
+                        break;
+                    case isPartTime:
+                        workHr = 4;
+                        break;
+                    default:
+                        workHr = 0;
+                        break;
+                }
+                empWage = empRatePrHr * workHr;
+                totalWage += empWage;
             }
-            totalWage = empRatePrHr * workHr;
-            Console.WriteLine("Employee total wage is " + totalWage);
+                Console.WriteLine("Employee total wage is " + totalWage);
         }
     }
 }
